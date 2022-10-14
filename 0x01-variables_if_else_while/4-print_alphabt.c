6d71b3d03c6019b5e0d16f6018474a121ab1092b
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-14 16:02:15 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Alway 0
+ */
+int main(void)
+{
+	int i;
+        Int a;
+
+        for( i = 'a'; i <= z; i++)
+	{
+		putchar("%c",i)
+	}
+        for( a = 'A'; i <= Z; A++)
+	{
+		putchar("%c",a)
+	}
+	return(0);
+}
