89ce4f7a3cc0b7c21117d22d3f31b678b9afa9ae
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-14 22:32:26 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+  int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar (n % 10 + '0');
+		n++;
+	}
+  int i;
+
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar (i);
+		i++;
+	}
+  putchar ('\n');
+	return (0);
+}
