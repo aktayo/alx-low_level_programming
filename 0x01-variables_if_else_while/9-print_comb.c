c078ab2f8490aefcc396fb246df32f566060cefd
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-14 22:47:23 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 9)
+	{
+		putchar (i % 10 + '0');
+    putchar (',');
+    putchar (' ');
+		i++;
+	}
+	putchar ( 9 % 10 + '0');
+	return (0);
+}
