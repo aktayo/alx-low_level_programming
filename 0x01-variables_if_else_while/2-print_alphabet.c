b5e62e1b4ac8d4fc454175ea9b3ab7214ae6e087
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-14 14:33:59 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+    int ch;
+  
+    for (ch = ‘a’; ch <= ‘z’; ch++)
+    putchar(ch);
+    putchar (‘\n’) ;
+    return (0);
+}
