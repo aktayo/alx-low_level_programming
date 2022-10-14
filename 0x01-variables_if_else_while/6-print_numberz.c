53f5e81631c35a4f0097a7e2ccc12ba6e99d3cdb
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-14 21:38:08 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+  
+	int i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		putchar (i);
+		i--;
+	}
+	putchar ('\n');
+	return (0);
+}
