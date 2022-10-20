e5d58163dda67945fc32050269ed3df601771774
akintayo <akintayo.odeniran@gmail.com>
2022-10-20 07:23:01 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -16,3 +16,15 @@ int _isdigit(int c)
 		return (0);
 	}
 }
+int main(void)
+{
+	 char c;
+
+	 c = '0';
+	 printf("%c: %d\n", c, _isdigit(c));
+
+	 c = 'a';
+	 printf("%c: %d\n", c, _isdigit(c));
+
+	 return (0);
+}
