ea5af37bb57f84f5a395995ce83b1b3ad8e1d693
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-20 16:06:50 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,12 @@
+#include "main.h"
+/**
+ *mul - multiply 2 variables
+ *@a: the variable
+ *@b: the variable
+ * Return: ret product of a and b
+ */
 
+int mul(int a, int b)
+{
+	return (a * b);
+}
