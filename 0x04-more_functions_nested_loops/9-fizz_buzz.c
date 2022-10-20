a0b87a640f6ca06ea5ad443460cac90c081fbf7a
akintayo <akintayo.odeniran@gmail.com>
2022-10-20 15:19:30 -0700

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 /**
  *print_square - print_square#
- *@size: no of times
+ *
  * Return: 0
  */
 
 int main(void)
 {
-  int i;
-  printf("1");
-  for (i = 0; i <= 100; i++)
-  {
-    printf(" ");
-    if (i % 3 == 0)
-        printf("Fizz");
-     if (i % 5 == 0)
-       printf("Buzz");
-    if (i % 3 != 0 && i % 5 != 0)
-      printf("%d", n);
-  }
-  printf("\n");
-    return (0);
+	int i;
+
+	printf("1");
+	for (i = 0; i <= 100; i++)
+	{
+		printf(" ");
+		if (i % 3 == 0)
+			printf("Fizz");
+
+		if (i % 5 == 0)
+			printf("Buzz");
+
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
 }
