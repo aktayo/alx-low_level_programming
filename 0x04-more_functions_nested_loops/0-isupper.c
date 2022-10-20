e76173a812fab07a16bbbaa9796400a70ddfdda3
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-20 14:35:29 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - Print all alphabet in lowercase
+ *
+ * Return: Alway 0
+ */
+
+int _isupper(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+  {
+    return (1);
+  }
+  else
+  {
+    return (0);
+  }
+}
+    
 
