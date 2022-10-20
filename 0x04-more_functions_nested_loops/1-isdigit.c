076d810f1c7994e500b2234bf33c38701dee60da
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-20 15:15:43 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Print all alphabet in lowercase
+ *@c: the variable
+ * Return: Alway 0
+ */
 
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
