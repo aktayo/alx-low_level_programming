fd31d08c678f1a864f2c1b30f57559f0cecd5d27
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-20 21:44:04 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *more_number - multiply 2 variables
+ *
+ * Return: ret product of a and b
+ */
+
+void print_line(int n)
+{
+  int i;
+  
+  if (n > 0)
+  {
+   for (i = 0; i < n; i++)
+     _putchar('_'); 
+  }
+  _putchar('\n');
+}
