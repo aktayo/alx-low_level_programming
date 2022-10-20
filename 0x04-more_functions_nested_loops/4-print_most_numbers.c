42df404bec319b3f542c9bf19e21a375555ca9ef
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-20 17:32:09 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_most_numbers - priint 0 - 9
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+  {
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+    if (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}
+		_putchar (i % 10 + '0');
+		i++;
+	}
+	_putchar ('\n');
+	return (0);
+}
