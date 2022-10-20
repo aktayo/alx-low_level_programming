18b8a6b393694fd47e20ec924cfa4d60032f18e4
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-20 14:42:27 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -17,4 +17,13 @@ int _isupper(int c)
   }
 }
     
+int main(void)
+{
+    char c;
 
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
