a0400f12907e7414bbff304378203ff7e052eaa9
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-21 23:02:16 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,9 @@ void print_diagonal(int n)
 		for (a = 0; a < n; a++)
 		{
 			for (b = 0; b < a; b++)
-                        {
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
-                        }
 		}
 	}
 }
