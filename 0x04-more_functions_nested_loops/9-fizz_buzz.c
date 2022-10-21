91d3fd527b4ab0d562db8302eb18ded27d8f911b
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-21 22:36:18 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+
 		if (i % 3 == 0)
 			printf("Fizz");
 
 		if (i % 5 == 0)
 			printf("Buzz");
 
-		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d", i);
 		printf(" ");
 	}
 	printf("\n");
