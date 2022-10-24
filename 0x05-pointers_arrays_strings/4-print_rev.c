38d8aa3c3398f6af51ab0402fc6a0e72510ba83b
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 14:45:15 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+ *print_rev - count the length of a string 
+ *@s: variable 
+ * Return: 0
+ */
 
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+i++;
+for(i = i - 1; i >= 0; i--)
+_putchar(s[i]);
+
+_putchar('\n');
+}
