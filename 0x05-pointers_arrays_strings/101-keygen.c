a3a9ca6d7b91d6c2da458bd529c1a8fdc31b82d7
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 22:34:13 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,17 @@
+void create_pass( char* randomString, size_t length )
+{
+    sizet_t i = 0;
+
+    srand(rand());
+    strcpy(randomString,"");
+
+    for( i = 0; i < (length/2); i++ )
+    {
+        *(randomString + i) = ( rand() % 25 + 65 );
+    }
+    for( ; i < length; i++ )
+    {
+        *(randomString + i) = ( rand() % 9 + 48 );
+    }
+} 
 
