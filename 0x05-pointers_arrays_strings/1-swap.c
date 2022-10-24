169d21d7dcc4ffb2c19a34cae51497eb03b2a814
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 13:46:03 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ *void reset_to_98 - Reset int to a new value 
+ *@n: variable value 
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int i;
+i = *a;
+*a = *b;
+*b = i;
+}
 
