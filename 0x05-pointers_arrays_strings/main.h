b817faa9269708abc320e92f302e820040bce268
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 13:19:40 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 void reset_to_98(int *n);
@@ -13,3 +14,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif /* MAIN_H */
