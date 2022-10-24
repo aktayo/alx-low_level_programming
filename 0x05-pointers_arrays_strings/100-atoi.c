554e5da85bf0ea8477d4b3d4fedeaa816759b6b4
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 22:25:48 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,20 @@
+void main()
+{
+    char input[100];
+    printf("Type a String which will be converted to an Integer: ");
+    scanf("%s", input);
 
+    int number = 0;
+    int neg = input[0] == '-';
+    int i = neg ? 1 : 0;
+    while ( input[i] >= '0' && input[i] <= '9' )
+    {
+      number *= 10;             // multiply number by 10
+      number += input[i] - '0'; // convet ASCII '0'..'9' to digit 0..9 and add it to number           
+      i ++;                     // step one digit forward
+    }
+    if ( neg )
+       number *= -1;
+
+    printf( "string %s -> number %d", input, number );
+}
