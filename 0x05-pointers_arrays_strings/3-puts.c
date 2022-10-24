78a023812c40f7540741a94b946416b504c8735d
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 14:17:49 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,16 @@
 
+#include "main.h"
+/**
+ *_puts - count the length of a string 
+ *@str: variable 
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(str++);
+}
+_putchar('\n');
+}
