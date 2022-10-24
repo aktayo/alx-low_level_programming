175cea674e5e019319bd03f2f4b67d7590eac7b1
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-24 14:03:19 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ *_strlen - count the length of a string 
+ *@s: variable 
+ * Return: ret length of s
+ */
 
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
