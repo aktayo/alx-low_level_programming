0860ceadde6af164048a215d5aee96b3cf3b8859
akintayo <akintayo.odeniran@gmail.com>
2022-10-24 10:24:37 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *void reset_to_98 - Reset int to a new value 
- *@n: variable value 
+ * void reset_to_98 - Reset int to a new value
+ * @n: variable value
  * Return: 0
  */
 
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
