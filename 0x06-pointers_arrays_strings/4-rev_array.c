c4b6d92eccaf958a19607354844c741e83434b20
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-27 15:30:35 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,3 +7,16 @@
  * @n: first n letters of 2nd string
  * Return: pointer to dest
  */
+void reverse_array(int *a, int n)
+{
+    int arr[n] = {9, 8, 7, 2, 4, 3};
+    int temp;
+    for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d,", arr[i]);
+    }
+}
