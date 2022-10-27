453bc897ec1b78f207d0562cf862f3117cfb4e22
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-27 15:18:28 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * _strcmp compares two strings values
+ * _strcmp - compares two strings values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
+ */
 
 int _strcmp(char *s1, char *s2)
 {
