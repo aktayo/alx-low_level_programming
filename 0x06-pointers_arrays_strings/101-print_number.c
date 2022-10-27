4e812ed8679743bac3d1661ac579837d941302b7
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-27 23:55:04 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,19 @@
 #include "main.h"
 
 /**
- * string_toupper - change the case of string
+ * print_number - change the case of string
  * @str: string to change it case
  * Return: str
  */
+void print_number(int n)
+{
+
+	n = 0;
+	while (n < 10)
+	{
+		_putchar(n % 10 + '0');
+		n++;
+	}
+	_putchar('\n');
+	return (0);
+}
