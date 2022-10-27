591f29970fc294433c48fbc9fe34027a01278748
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-27 12:12:10 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * _strcat - concatennates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
+
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+
+i = 0;
+while (dest[i] != '\0')
+
+}
+
+j = 0;
+
+while (src[j] != '\0')
+
+dest[i] = src[j];
+
+dest[i]
