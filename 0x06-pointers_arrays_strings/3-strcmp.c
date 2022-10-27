fc38ac3e6736a25a23bc84297cd3b803dcc5ae98
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-27 14:47:27 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,12 @@
  * @s2: string 2
  * Return: the difference in s1 and s2
 
-int strcmp(char *s1, char *52)
+int strcmp(char *s1, char *s2)
 {
 int i;
 
 i = 0;
-while (s1[i] != '\0' && $2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
