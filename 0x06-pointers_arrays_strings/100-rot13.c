b6d87cdda49d26bb9d821d774cbd77ab1c396b1c
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-27 23:42:19 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,2 +1,31 @@
+int i,diff[i];  // Bad
 
-tr '[A-Za-z]' [N-ZA-Mn-za-m]
+
+// for(i=0;i<'\0';i++){
+for(i=0; string[i]; i++){
+
+
+//string[i]<='a' && string[i]<='z'
+string[i]>='a' && string[i]<='z'
+
+
+// if(...  || !isspace(string[i])) {
+if(...){
+
+
+  //      for(i=0;i<'\0';i++){
+  //          if(string[i]<='a' && string[i]<='z' || string[i]>='A' && string[i]<='Z'||!isspace(string[i])){
+  //  string[i]=tolower(string[i]);
+  //}
+
+  for(i=0; string[i]; i++){
+    string[i]=tolower((unsigned char) string[i]);
+  }
+  int diff[i];  // Now define `diff[]`
+
+
+
+  
+
+
+
