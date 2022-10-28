6e36cc671f2a300c9fde9fe71e587072435281bf
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-28 11:27:21 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,13 @@
+#include "main.h"
+
+/**
+ * infinite_add - concatennates two strings
+ * @n1: copy to
+ * @n2: nth string
+ * @r: result
+ * @size_r: size of buffer r
+ * Return: r
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 r = n1 + n2;
