977645f8a2c0cd74428f5eb9d824398e0592638b
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-28 10:35:22 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -8,13 +7,13 @@
  */
 void print_number(int n)
 {
-
-	n = 0;
-	while (n < 10)
-	{
-		_putchar(n % 10 + '0');
-		n++;
-	}
-	_putchar('\n');
-	return (0);
+if (n < 0)
+{
+_putchar('-');
+n = n * -1;
+}
+if (n == 0)
+_putchar('0');
+if (n/10)
+_putchar(n % 10 + '0');
 }
