10b4f970a07f67b9ae917412887b2cff505a6de6
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-31 23:07:33 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,43 +1,17 @@
-#include "main.h"                                                                                                                   
+#include "main.h"
+/**
+* _memset - Fills the first n bytes of the memory area
+ *          pointed to by @s with the constant byte @c
+ * @s: A pointer to the memory area to be filled
+ * @c: The character to fill the memory area with
+ * @n: The number of bytes to be filled.
+ * Return: A pointer to the filled memory area
+*/
 
-                                                                                                                                     
-
-/**                                                                                                                                 
-
- * _memset - Fills the first n bytes of the memory area                                                                             
-
- *          pointed to by @s with the constant byte @c.                                                                             
-
- * @s: A pointer to the memory area to be filled.                                                                                   
-
- * @c: The character to fill the memory area with.                                                                                   
-
- * @n: The number of bytes to be filled.                                                                                             
-
- * description _memset: over there                                                                                                   
-
- *                                                                                                                                   
-
- * Return: A pointer to the filled memory area @s.                                                                                   
-
- *                                                                                                                                   
-
- */                                                                                                                                 
-
-char *_memset(char *s, char b, unsigned int n)                                                                                              
-
-{                                                                                                                                    
-
-        unsigned int a;                                                                                                          
-
-                                                                                                
-
-        for (a = 0; a < n; a++)                                                                                          
-
-                s[a] = b;                                                                                              
-
-                                                                                                                                     
-
-        return (s);                                                                                                            
-
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+s[a] = b;
+return (s);
 }
