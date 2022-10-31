5c5c34599f6d52fc39ef13233ebd65650700a8d5
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-31 22:13:02 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,26 +7,57 @@
  * Return: If the substring is located - a pointer to the beginning
  */
 
-char *_strstr(char *haystack, char *needle)
-{
-int index;
 
-if (*needle == 0)
-return (haystack);
+char *_strstr(char *haystack, char *needle)                                                                                        
 
-while (*haystack)
-{
-index = 0;
-if (haystack[index] == needle[index])
-{
-do {
-if (needle[index + 1] == '\0')
-return (haystack);
-index++;
-}
-while (haystack[index] == needle[index]);
-}
-haystack++;
-}
-return ('\0');
+{                                                                                                                                 
+
+        int index;                                                                                                                
+
+                                                                                                                                   
+
+        if (*needle == 0)                                                                                                          
+
+                return (haystack);                                                                                                
+
+                                                                                                                                   
+
+        while (*haystack)                                                                                                          
+
+        {                                                                                                                          
+
+                index = 0;                                                                                                        
+
+                                                                                                                                   
+
+                if (haystack[index] == needle[index])                                                                              
+
+                {                                                                                                                  
+
+                        do {                                                                                                      
+
+                                if (needle[index + 1] == '\0')                                                                    
+
+                                        return (haystack);                                                                        
+
+                                                                                                                                   
+
+                                index++;                                                                                          
+
+                                                                                                                                   
+
+                        } while (haystack[index] == needle[index]);                                                                
+
+                }                                                                                                                  
+
+                                                                                                                                   
+
+                haystack++;                                                                                                        
+
+        }                                                                                                                          
+
+                                                                                                                                   
+
+        return ('\0');                                                                                                            
+
 }
