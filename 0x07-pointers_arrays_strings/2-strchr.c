dfb283048df126615fcf2ec1058a750d82347cc8
aktayo <113802719+aktayo@users.noreply.github.com>
2022-10-31 20:59:05 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,36 +1,54 @@
-#include "main.h"                                                                                                                   
+#include "main.h"
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to be searched.
+ * @c: The character to be located
+ * Return: If c is found - a pointer to the first occurence.
+ */
 
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+
+
+
+
+                                                                                                                
                                                                                                                                      
 
 /**                                                                                                                                 
 
- * _strchr - Locates a character in a string.                                                                                       
+.                                                                                       
 
- * @s: The string to be searched.                                                                                                   
+                                                                                                   
 
- * @c: The character to be located.                                                                                                 
+.                                                                                                 
 
  *                                                                                                                                   
 
- * Return: If c is found - a pointer to the first occurence.                                                                         
+                                                                         
 
  *         If c is not found - NULL.                                                                                                 
 
  */                                                                                                                                 
 
-char *_strchr(char *s, char c)                                                                                                      
+                                                                                                      
 
 {                                                                                                                                    
 
-        int index;                                                                                                                  
+                                                                                                                          
 
                                                                                                                                      
 
-        for (index = 0; s[index] >= '\0'; index++)                                                                                  
+                                                                                          
 
         {                                                                                                                            
 
-                if (s[index] == c)                                                                                                  
+                                                                                                                  
 
                         return (s + index);                                                                                          
 
