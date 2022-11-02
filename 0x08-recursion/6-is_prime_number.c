323bd456e9d7482210bc928201e953070e788db1
aktayo <113802719+aktayo@users.noreply.github.com>
2022-11-02 21:29:20 +0000

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,9 +2,7 @@
 
                                                                                                                                      
 
-int is_divisible(int num, int div);                                                                                                  
-
-int is_prime_number(int n);                                                                                                          
+                                                                                                      
 
                                                                                                                                      
 
