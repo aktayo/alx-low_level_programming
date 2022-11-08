aa33e87a28ae384a2dc513f9158c165d0c4e6129
aktayo <113802719+aktayo@users.noreply.github.com>
2022-11-08 21:15:46 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,24 @@
 #include "main.h"
+/**
+ *_strlen - count the length of a string
+ *@s: variable
+ * Return: ret length of s
+ */
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+{
+	len++;
+	s++;
+}
+return (len);
+}
+
+#include "main.h"
 
 /**
  * _strncpy - concatennates two strings
