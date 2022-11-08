1928142ba7ee6b0616310573a1f6fbbeb2691fdf
aktayo <113802719+aktayo@users.noreply.github.com>
2022-11-08 21:12:42 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * _strncpy - concatennates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: first n letters of 2nd string
+ * Return: pointer to dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
