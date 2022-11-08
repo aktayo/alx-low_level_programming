6e6059e16c82651197a3789b7a1c0e43119d3400
aktayo <113802719+aktayo@users.noreply.github.com>
2022-11-08 21:22:18 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,6 +19,27 @@ return (len);
 }
 
 #include "main.h"
+/**
+*_strcpy - copies the string pointed to by src
+*@dest: destination
+*@src: source
+* Return: string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++;
+	}
+	*(dest + len) = '\0';
+	return (dest);
+}
+
+#include "main.h"
 
 /**
  * _strncpy - concatennates two strings
