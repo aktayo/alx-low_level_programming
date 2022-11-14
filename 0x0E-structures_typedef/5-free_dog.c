cb40dd92b9daac50c90ce3edd65bd042e2161602
aktayo <113802719+aktayo@users.noreply.github.com>
2022-11-14 14:31:28 +0000

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,29 +1,20 @@
 #include "dog.h"
-
 #include <stdlib.h>
 
-
 /**
-
  * free_dog - Frees dogs.
-
  * @d: The dog to be freed.
-
  */
 
 void free_dog(dog_t *d)
-
 {
+	if (d == NULL)
+		return;
 
-        if (d == NULL)
+	free(d->owner);
 
-                return;
+	free(d->name);
 
-
-        free(d->owner);
-
-        free(d->name);
-
-        free(d);
+	free(d);
 
 }
