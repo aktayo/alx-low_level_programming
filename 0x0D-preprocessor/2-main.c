f8c737a4897b080907d5d51d9543883ebd6e4311
aktayo <113802719+aktayo@users.noreply.github.com>
2022-11-14 13:46:26 +0000

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
-
 /**
-
  * main - Prints the name of the file of the program
-
  *        was compiled from, followed by a new line.
-
- * Return: Always 0.
-
+ * Return: Always 0
  */
 
 int main(void)
-
 {
-
-        printf("%s\n", __FILE__);
-
-
-        return (0);
-
+printf("%s\n", __FILE__);
+return (0);
 }
